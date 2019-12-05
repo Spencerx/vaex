3316b2ac50b627a18dbe383317f1d61e3150e80a
Jovan Veljanoski <jovan.veljanoski@gmail.com>
2019-12-05 11:38:38 +0100

    fix(test) Fix an buggy test in evaluate

--- tests/evaluate_test.py
+++ tests/evaluate_test.py
@@ -9,5 +9,5 @@ def test_evaluate_function_filtered_df():
     df = vaex.from_arrays(x=np.arange(10))
     df_filtered = df[df.x!=4]
     df_filtered.add_function('custom_function', custom_func)
-    df_filtered['y'] = df_filtered.func.custom_function(dff.x)
+    df_filtered['y'] = df_filtered.func.custom_function(df_filtered.x)
     assert df_filtered.y.tolist() == [0, 1, 4, 9, 25, 36, 49, 64, 81]
